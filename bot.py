83b0e265f3198e2507868ee2f0de76a834078475
Nathan Hare <nhare330@gmail.com>
2015-10-16 11:31:00 -0400

    Enable multiple twitter feeds to be tracked

--- bot.py
+++ bot.py
@@ -17,7 +17,10 @@ realname = "Twitter Relay bot, owned by treetrunk"
 passwd = 'twitterbot'
 port = 6667
 owner = 'treetrunk'
-tweeter = 'username'
+tweeters = [
+  'username1',
+  'username2'
+]
 
 # Connect to server and set nick
 irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -27,30 +30,36 @@ irc.send("NICK %s \r\n" % nick)
 irc.send("USER %s %s %s :%s \r\n" % (nick, nick, nick, realname))
 irc.send("JOIN %s \r\n" % chan)
 
-def get_tweets(tweeter):
+def get_tweets(tweeters):
   
   time.sleep(5)
-  page = urllib2.urlopen('https://twitter.com/'+ tweeter).read()
-  lines = page.split("\n")
-  tweets = []
+  tweets = {}
+  stop = ""
 
-  for line in lines:
+  for tweeter in tweeters:
 
-    if line.find('<p class="TweetTextSize TweetTextSize--26px js-tweet-text tweet-text" lang="en" data-aria-label-part="0">') != -1:
-      
-      words = line.split()
+    page = urllib2.urlopen('https://twitter.com/'+ tweeter).read()
+    lines = page.split("\n")
 
-      for word in words:
-        if word.find('http://t.co/') != -1:
-          url_text = word.split('"')
-          url = url_text[1]
+    for line in lines:
+      if stop != tweeter:
 
-      no_html = re.compile(r'<[^>]+>')
-      line = line.encode('ascii', errors='ignore')
-      tweet = no_html.sub('', line)
-      tweet = " ".join(tweet.split())
-      tweets.append({"text":tweet, "url":url})
-  
+        if line.find('<p class="TweetTextSize TweetTextSize--26px js-tweet-text tweet-text" lang="en" data-aria-label-part="0">') != -1:
+          
+          words = line.split()
+
+          for word in words:
+            if word.find('http://t.co/') != -1:
+              url_text = word.split('"')
+              url = url_text[1]
+
+          no_html = re.compile(r'<[^>]+>')
+          line = line.encode('ascii', errors='ignore')
+          tweet = no_html.sub('', line)
+          tweet = " ".join(tweet.split())
+          tweets[tweeter] = {"text":tweet, "url":url}
+          stop = tweeter
+    
   return tweets
 
 old_tweet = ""
@@ -62,10 +71,16 @@ while True:
   data = irc.recv (4096)
   if data.find('PING') != -1:
     irc.send('PONG ' + data.split() [1] + '\r\n')
-  print data
 
-  tweets = get_tweets(tweeter)
-  if tweets[0]['text'] != old_tweet:
-    old_tweet = tweets[0]['text']
-    irc.send("PRIVMSG %s : *** Tweet from @%s *** %s -- LINK: %s \r\n" % (chan, tweeter, tweets[0]['text'], tweets[0]['url']))
-    print "*** Tweet from @%s *** %s -- LINK: %s \r\n" % (tweeter, tweets[0]['text'], tweets[0]['url'])
+  old_tweets = {}
+  tweets = get_tweets(tweeters)
+
+    
+  for tweeter in tweeters:
+    if tweeter in old_tweets:
+      if old_tweets[tweeter] != tweets[tweeter]:
+        irc.send("PRIVMSG %s : *** Tweet from @%s *** %s -- LINK: %s \r\n" % (chan, tweeter, tweets[tweeter]['text'], tweets[tweeter]['url']))
+        print "*** Tweet from @%s *** %s -- LINK: %s \r\n" % (tweeter, tweets[tweeter]['text'], tweets[tweeter]['url'])
+    
+  old_tweets = tweets
+  #print "NOT SENDING TWEETS:\r\n", old_tweets
